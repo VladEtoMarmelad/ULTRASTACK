fb5c295f6057bd551662460facfdcac8556dd354
VladEtoMarmelad <vladiksehovcov@gmail.com>
2026-04-01 19:18:47 +0300

    style: use TechUI.H3 instead of TechUI.H1 in techPages

--- ultrastack-frontend/components/techPages/NextNavigationTech.tsx
+++ ultrastack-frontend/components/techPages/NextNavigationTech.tsx
@@ -2,7 +2,7 @@ import { TechUI } from "../index"
 
 export const NextNavigationTech = () => (
   <>
-    <TechUI.H1>You can test some Nextjs Navigation links here</TechUI.H1>
+    <TechUI.H3>You can test some Nextjs Navigation links here</TechUI.H3>
     <TechUI.P>
       Try switching to <TechUI.A href="/?tech=nextjs">another technology</TechUI.A> or check out the{" "}
       <TechUI.A href="/alternate">Next Navigation alter page</TechUI.A>.
--- ultrastack-frontend/components/techPages/NextjsTech.tsx
+++ ultrastack-frontend/components/techPages/NextjsTech.tsx
@@ -2,7 +2,7 @@ import { TechUI } from "../index"
 
 export const NextjsTech = () => (
   <>
-    <TechUI.H1>To get started, edit the page.tsx file.</TechUI.H1>
+    <TechUI.H3>To get started, edit the page.tsx file.</TechUI.H3>
     <TechUI.P>
       Looking for a starting point or more instructions? Head over to{" "}
       <TechUI.A href="https://vercel.com/templates">Templates</TechUI.A> or the Learning center.
